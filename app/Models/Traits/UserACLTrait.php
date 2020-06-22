cef03cbff7ead4effa51061828274e19abfb5208
Maurilio <mbolonhesi@hotmail.com>
2020-06-22 10:03:08 -0300

    22/06/2020 - mod 18 - aula1

--- app/Models/Traits/UserACLTrait.php
+++ app/Models/Traits/UserACLTrait.php
@@ -1,67 +1,68 @@
-<?php 
- namespace App\Models\Traits;
+<?php
+
+namespace App\Models\Traits;
 
 use App\Models\Tenant;
 
-/**
-  * 
-  */
- trait UserACLTrait
- {
-     public function permissions():array //PERMISSOES DOS USUÁRIOS (PLANO x CARGO)
-     {       
+trait UserACLTrait
+{
+    public function permissions(): array
+    {
         $permissionsPlan = $this->permissionsPlan();
         $permissionsRole = $this->permissionsRole();
 
-        $permissions=[];
-        foreach ($permissionsRole as $permission){
-            if (in_array($permission,$permissionsPlan)){
-                array_push($permissions,$permission);
-            }
+        $permissions = [];
+        foreach ($permissionsRole as $permission) {
+            if (in_array($permission, $permissionsPlan))
+                array_push($permissions, $permission);
         }
+
         return $permissions;
-     }
+    }
 
-     public function PermissionsPlan():array //método que retorna todas as permissoes do plano
-     {
-        //$tenant = $this->tenant()->first();
-        //$plan = $tenant->plan()->first();
-        $tenant = Tenant::with('plan.profiles.permissions')->where('id',$this->tenant_id)->first();
-
+    public function permissionsPlan(): array
+    {
+        // $tenant = $this->tenant;
+        // $plan = $tenant->plan;
+        $tenant = Tenant::with('plan.profiles.permissions')->where('id', $this->tenant_id)->first();
         $plan = $tenant->plan;
 
-        $permissions=[];
-       foreach ($plan->profiles as $profile){
-           foreach ($profile->permissions as $permission){
-               array_push($permissions,$permission->name);
-           }
-       }
-       return $permissions;
-     }
+        $permissions = [];
+        foreach ($plan->profiles as $profile) {
+            foreach ($profile->permissions as $permission) {
+                array_push($permissions, $permission->name);
+            }
+        }
 
-     public function permissionsRole():array //metodo retorna todas as permissoes do cargo
-     {
+        return $permissions;
+    }
+
+    public function permissionsRole(): array
+    {
         $roles = $this->roles()->with('permissions')->get();
 
-        $permissions=[];
-        foreach ($roles as $role){
-            foreach ($roles->permissions as $permission){
-                array_push($permissions,$permission->name);
+        $permissions = [];
+        foreach ($roles as $role) {
+            foreach ($role->permissions as $permission) {
+                array_push($permissions, $permission->name);
             }
-        }           
-       return $permissions;
-     }
+        }
 
-     public function hasPermission(string $permissionName):bool{
-        return in_array($permissionName,$this->permissions());
-     }
+        return $permissions;
+    }
 
-     public function isAdmin():bool{
-          return in_array($this->email, config('acl.admins'));
-     }
+    public function hasPermission(string $permissionName): bool
+    {
+        return in_array($permissionName, $this->permissions());
+    }
 
-     public function isTenant():bool{
+    public function isAdmin(): bool
+    {
+        return in_array($this->email, config('acl.admins'));
+    }
+
+    public function isTenant(): bool
+    {
         return !in_array($this->email, config('acl.admins'));
-   }
- }
- 
+    }
+}
